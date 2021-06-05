99fca92bf13945e1e40f4bbb68cd85c3c1d97976
Rana <r-saber@nlaw.northwestern.edu>
2021-06-04 20:52:48 -0500

    jinja inheritence

--- static/js/group_bar.js
+++ static/js/group_bar.js
@@ -7,11 +7,11 @@ console.log(birthRate1819);
 */
 
 // Use an anon function to return only the birth rates for a specific age range variable
-var rates1517 = birthRate1517.map(function(d) { return d.rate;});
-var rates1819 = birthRate1819.map(function(d) { return d.rate;});
+var rates1517 = birthRate1517.map(function (d) { return d.rate; });
+var rates1819 = birthRate1819.map(function (d) { return d.rate; });
 
 // Filter JSON to return only the years (could use either variable)
-var years = birthRate1517.map(function(d) { return d.year;});
+var years = birthRate1517.map(function (d) { return d.year; });
 
 // Inserting data into Plotly chart 
 var trace1 = {
@@ -19,21 +19,22 @@ var trace1 = {
   y: rates1819,
   name: 'Ages 18-19',
   type: 'bar',
-  marker: {color:'#7f7f7f'}
+  marker: { color: '#7f7f7f' }
 };
 console.log(trace1);
 
 var trace2 = {
   x: years,
-  y:rates1517,
+  y: rates1517,
   name: 'Ages 15-17',
   type: 'bar',
-  marker: {color:'#17becf'}
+  marker: { color: '#17becf' }
 };
 
 
 var data = [trace1, trace2];
 
-var layout = {barmode: 'group', title: 'US Teen Birth Rate by Age Group', yaxis: {title: "Birth Rate"}, xaxis: {title:"Years", tickmode :"linear"}};
+var layout = { barmode: 'group', title: 'US Teen Birth Rate by Age Group', yaxis: { title: "Birth Rate" }, xaxis: { title: "Years", tickmode: "linear" } };
 Plotly.newPlot('plot', data, layout);
+
 
